199540c60955c5c8e167804822c2d73cb9aea6f8
Cesar <cmiranda956@gmail.com>
2023-01-21 23:22:10 -0600

    vim airline  installation

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -168,6 +168,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/cesar/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
     url = "https://github.com/folke/tokyonight.nvim"
+  },
+  ["vim-airline"] = {
+    loaded = true,
+    path = "/home/cesar/.local/share/nvim/site/pack/packer/start/vim-airline",
+    url = "https://github.com/vim-airline/vim-airline"
   }
 }
 
